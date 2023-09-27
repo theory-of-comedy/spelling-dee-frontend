6480e566b7b0b1b4104556ac93007f9f547758f4
Rosemarries <rosemarries.404notfound@gmail.com>
2023-09-28 02:06:07 +0700

    feat: eslint and prettier

--- src/App.jsx
+++ src/App.jsx
@@ -15,19 +15,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-
-
-
-
-
           Learn React
         </a>
-                        </header>
-
-
-
-
-
+      </header>
     </div>
   );
 }
