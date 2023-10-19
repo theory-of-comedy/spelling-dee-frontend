cd710ccf22feed628ce45d42fa0fb06c14a14feb
Sirasit Thiancharoenchai <ootcazy007@gmail.com>
2023-10-19 23:39:10 +0700

    feat: css

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 
 function Root() {
   return (
-    <div className="flex justify-center min-h-fit w-screen h-screen items-center bg-main-bg text-white bg-no-repeat bg-center bg-cover">
-      <div className="flex items-center w-full flex-wrap-reverse">
-        <div className="lg:pl-32 md:pl-16 pl-8 sm:w-[60%] font-Poppins">
+    <div className="flex justify-center min-h-screen w-screen items-center bg-main-bg text-white bg-no-repeat bg-center bg-cover">
+      <div className="flex items-center w-full flex-wrap-reverse my-10">
+        <div className="lg:pl-32 md:pl-16 px-8 sm:w-[60%] font-Poppins">
           <h1 className="text-[#FFD747] font-bold mb-10 lg:text-3xl md:text-2xl text-xl">
             SPELLING DEE DA VINCI CODE
           </h1>
